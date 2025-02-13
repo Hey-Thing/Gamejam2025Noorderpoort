435142fb4c917e7429a00df0ea0edd69a3de1979
Billel <85782585+Billel-Game@users.noreply.github.com>
2025-02-13 13:56:29 +0100

    a
    a

--- GJ/Assets/Billel/Scripts/GameController.cs
+++ GJ/Assets/Billel/Scripts/GameController.cs
@@ -15,7 +15,7 @@ public class GameController : MonoBehaviour
     public void StartGame()
     {
         Debug.Log("Game Started for " + playerName);
-        SceneManager.LoadScene("Dag");
+        SceneManager.LoadScene("damian");
     }
 
     public void AddScore(int points)
